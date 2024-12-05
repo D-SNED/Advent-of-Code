e0cc38a7874a5185db68746b85314fa3b7b47f39
DSnediker <dsnediker7@gmail.com>
2024-12-04 18:41:59 -0800

    solved day 2. Need to refactor code. It's very inefficient

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -1,25 +1,67 @@
-def total_between_lists():
-    with open("2024/day_1.txt", "r") as file:
-        data = file.read()
-        data = data.split()
-        l1 = []
-        l2 = []
-
-        for i in range(len(data)):
-            if i % 2 == 0:
-                l1.append(data[i])
-            else:
-                l2.append(data[i])
-
-        l1 = sorted(l1)
-        l2 = sorted(l2)
-
-        res = 0
-
-        for i in range(len(l1)):
-            res += abs(int(l1[i]) - int(l2[i]))
-
-        return res
+with open("2024/day_1.txt", "r") as file:
+    data = file.read()
+    data = data.split()
 
 
-print(total_between_lists())
+def total_between_lists(data):
+    l1 = []
+    l2 = []
+
+    for i in range(len(data)):
+        if i % 2 == 0:
+            l1.append(data[i])
+        else:
+            l2.append(data[i])
+
+    l1 = sorted(l1)
+    l2 = sorted(l2)
+
+    res = 0
+
+    for i in range(len(l1)):
+        res += abs(int(l1[i]) - int(l2[i]))
+
+    return res
+
+
+# find how many times number in left list occurs in right list
+# multiply left list number by number if occurences
+# add up similarity scores
+
+
+def find_similarity_score(data):
+    l1 = []
+    l2 = []
+
+    for i in range(len(data)):
+        if i % 2 == 0:
+            l1.append(data[i])
+        else:
+            l2.append(data[i])
+
+    l1 = sorted(l1)
+    l2 = sorted(l2)
+
+    similarity_dict = {}
+
+    for i in range(len(l1)):
+        if l1[i] not in similarity_dict:
+            similarity_dict[l1[i]] = 0
+
+    for i in range(len(l2)):
+        if l2[i] in similarity_dict:
+            similarity_dict[l2[i]] += 1
+
+    similarity_total = 0
+
+    print(similarity_dict)
+
+    for key in similarity_dict:
+        if similarity_dict[key] > 0:
+            similarity_total += int(key) * similarity_dict[key]
+
+    return similarity_total
+
+
+# print(total_between_lists(data))
+print(find_similarity_score(data))
